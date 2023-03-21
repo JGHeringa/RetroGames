3c8a655b0306377a31d928059333e956bd95c042
Duran <87602@glr.nl>
2023-03-21 15:51:02 +0100

    push

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'includes/head.php';
 ?>
     <body>
 
-        <h1>retrogames</h1>
+        <h1>Retrogames</h1>
         <p>This is a paragraph.</p>
         <div id="result">
         </div>
