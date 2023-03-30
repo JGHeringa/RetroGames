994f7113f1e7738643b06ddac04dc2d20ba49b45
Duran <87602@glr.nl>
2023-03-30 13:56:17 +0200

    Fixed grid system works on mobile, The card styling is almost done just need to add a few details

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ require 'includes/config.php';
 include 'includes/head.php';
 ?>
     <body>
-
-        <h1>Retrogames</h1>
-        <p>This is a paragraph.</p>
-        <div id="result">
+    <div class="container mx-auto" style="max-width: 960px;">
+        <h1 class="text-center my-5">Retrogames</h1>
+        <div class="row" id="result">
         </div>
+    </div>
     </body>
 <?php
 include 'includes/foot.php';
--- js/app.js
+++ js/app.js
@@ -1,22 +1,33 @@
 function uitlees() {
-    $.ajax({
-        type: "GET",
-        url: "./php/getGames.php",
-        dataType: "JSON",
-        success: function (games) {
-            let output = "";
-            output += "<ul>";
-            for (let i = 0; i < games.length; i++) {
-                output += "<li>" + games[i].titel + " " + games[i].omschrijving +"</li>";
-            }
-            output += "</ul>";
-            $("#result").html(output);
-        },
-        error: function (xhr, status, error) {
-            console.log("Error: " + error);
+  $.ajax({
+    type: "GET",
+    url: "./php/getGames.php",
+    dataType: "JSON",
+    success: function (games) {
+      let output = "";
+      for (let i = 0; i < games.length; i++) {
+        if (i % 4 == 0) {
+          output += '<div class="w-100"></div>';
         }
-    });
+        output += `<div class="col-sm-6 col-md-3 mb-4">
+                        <div class="card bg-danger">
+                          <h5 class="bg-primary p-3 text-white card-title mt-0">${games[i].titel}</h5>
+                          <img src="${games[i].foto}" class="card-img-top rounded-0">
+                          <div class="card-body">
+                            <p class="card-text">${games[i].omschrijving}</p>
+                            <a href="#" class="btn btn-dark rounded-0 mb-0 w-100">meer</a>
+                          </div>
+                        </div>
+                      </div>`;
+      }
+      $("#result").html(output);
+    },
+    error: function (xhr, status, error) {
+      console.log("Error: " + error);
+    },
+  });
 }
+
 $(document).ready(function () {
-    uitlees();
+  uitlees();
 });
